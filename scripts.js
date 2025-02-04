86a65874e5ab4e2254aa911c26bf884be1caafc3
Yasha261998 <49835962+Yasha261998@users.noreply.github.com>
2025-02-04 09:41:17 +0200

    Ending main layout

--- scripts.js
+++ scripts.js
@@ -1,11 +1,60 @@
-document.getElementById("copyright-year").textContent = new Date().getFullYear();
+/* update of year */
+
+document.getElementById("copyright-year").textContent =
+  new Date().getFullYear();
 
 // header event scroll
-window.addEventListener("scroll", function() {
-    let header = this.document.querySelector("header");
-    if (this.window.scrollY > 50) {
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
+window.addEventListener("scroll", function () {
+  let header = this.document.querySelector("header");
+  if (this.window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+// sending form
+const formContact = document.getElementById("contact-form");
+const resultForm = document.getElementById("result-form");
+const submitButton = document.getElementById("btn-form");
+const textButton = submitButton.textContent;
+
+formContact.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(formContact);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  submitButton.disabled = true; // disable submit button
+  submitButton.textContent = "Sending...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        resultForm.innerHTML = json.message;
+      } else {
+        // console.log(response);
+        resultForm.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      // console.log(error);
+      resultForm.innerHTML = "Error sending the data of form!";
+    })
+    .then(function () {
+      submitButton.disabled = false;
+      submitButton.textContent = textButton;
+
+      formContact.reset();
+      setTimeout(() => {
+        resultForm.style.display = "none";
+      }, 3000);
+    });
 });
