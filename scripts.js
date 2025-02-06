edb4715bb346dec0092a390abde797ec02ed9645
Yasha261998 <49835962+Yasha261998@users.noreply.github.com>
2025-02-06 17:22:35 +0200

    update burger js

--- scripts.js
+++ scripts.js
@@ -63,7 +63,7 @@ formContact.addEventListener("submit", function (e) {
 /* burger menu */
 
 function openCloseMenu() {
-  this.classList.toggle("active");
+  document.querySelector(".header .burger").classList.toggle("active");
   document.querySelector("nav").classList.toggle("open");
   document.querySelector("body").classList.toggle("no-scroll");
 }
